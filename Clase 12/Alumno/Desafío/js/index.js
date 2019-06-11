74139840123a9a8b2c59eb030af516d59ac7ace8
cmartinobelay <cmartinobelay@gmail.com>
2019-06-11 01:26:53 -0300

    Desafio 12

--- Clase 12/Alumno/Desafío/js/index.js
+++ Clase 12/Alumno/Desafío/js/index.js
@@ -1,1 +1,20 @@
 console.log('Hola')
+
+var emailImput = $('#email')
+
+ emailImput.blur(validateEmailField)
+
+ function validateEmailField (event){
+   var inputeNode = $(this)
+   var value = inputeNode.val()
+
+   if (!value || value.indexof('@') === -1 || value.indexof('.') === -1){
+     inputeNode.removeClass('is-valid')
+     inputeNode.addClass('is- invalid')
+   } else {
+     inputeNode.removeClass('is-invalid')
+     inputeNode.addClass('is-valid')
+   }
+
+ }
+
